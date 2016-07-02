442b9db63086388fa6ea8e9b498937210f03b565
manu_r <manuel.ritz@hs-augsburg.de>
2016-07-02 10:53:06 +0200

    add Fehlerhaften Login

--- public/controllers/signup.js
+++ public/controllers/signup.js
@@ -2,7 +2,7 @@ angular.module('MyApp')
     .controller('SignupCtrl', ['$scope', 'Auth', function($scope, Auth) {
         $scope.signup = function() {
             Auth.signup({
-                email: $scope.email,
+                username: $scope.email,
                 password: $scope.password
             });
         };
